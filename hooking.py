93886d99467bf4b13dbe925c509edd43508bd45e
waruenada <toon-tam@hotmail.com>
2021-04-21 09:57:36 +0700

    add select_color

--- hooking.py
+++ hooking.py
@@ -216,6 +216,7 @@ class Hooking(Resource):
                     }
                 ]
         }
+        return this_quest
 
     # core api
     def post(self):
@@ -252,24 +253,25 @@ class Hooking(Resource):
         print(TAG, "user_id=", user_id)
         print(TAG, "one email=", email)
 
-        msg_type = data["message"]["type"]
-
-        if(msg_type == "text"):
-            my_msg = data["message"]["text"]
-            print(TAG, "my_msg=", my_msg)
-            if(my_msg == "pink"):
-                print(TAG, "hi recv")
-                self.send_msg(one_id,"I like Pink!!")
-
-        # if(select_color in data['message']):
-        #     # select_color = data['message']['data']['select_color']
-        #     if(select_color == "pink"):
-        #         print(TAG,"color=",select_color)
-        #         self.send_msg(one_id,"pink!!")
+        # msg_type = data["message"]["type"]
         #
-        #     elif(select_color == "green"):
-        #         print(TAG,"color",select_color)
-        #         self.send_msg(one_id,"green!!")
+        # if(msg_type == "text"):
+        #     my_msg = data["message"]["text"]
+        #     print(TAG, "my_msg=", my_msg)
+        #     if(my_msg == "pink"):
+        #         print(TAG, "hi recv")
+        #         self.send_msg(one_id,"I like Pink!!")
+
+        if("select_color" in data['message']['data']):
+            select_color = data['message']['data']['select_color']
+            if(select_color == "pink"):
+                print(TAG,"select_color=",select_color)
+                self.send_msg(one_id,"Pink Pink!!")
+                return module.success()
+            elif(select_color == "green"):
+                print(TAG,"select_color=",select_color)
+                self.send_msg(one_id,"Green Green!!")
+                return  module.success()
 
 
         # user_exist = self.is_user_exist(email)
