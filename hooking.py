32cdb2a00554b17d8b9756237b7bcd7f9cb13898
waruenada <toon-tam@hotmail.com>
2021-04-22 12:04:51 +0700

    add users age

--- hooking.py
+++ hooking.py
@@ -320,9 +320,9 @@ class Hooking(Resource):
                     self.send_quick_reply(one_id, req_body)
                     return module.success()
                 elif(age is None):
-                    self.send_msg(one_id, "คุณอายุเท่าไหร่? กรุณาระบุเป็นตัวเลข")
                     age = data['message']['text']
                     age = int(age)
+                    self.send_msg(one_id, "คุณอายุเท่าไหร่? กรุณาระบุเป็นตัวเลข")
                     if(age > 0):
                         cmd = """UPDATE `users` SET `age` = '%s' WHERE `users`.`one_email` = '%s'""" % (age, email)
                         update = self.update_data(cmd)
