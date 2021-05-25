62659055354c2eee5be95a778eb925203f9152ca
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-05-25 14:02:02 +0700

    edit android debug

--- hooking.py
+++ hooking.py
@@ -92,7 +92,7 @@ class Hooking(Resource):
                                                 "---------------------------" + "\n" +
                                                 "uuid : " + data['uuid'] + "\n" +
                                                 "major : " + data['major'] + "\n" + 
-                                                "minor : " + data['minor'] + "\n"
+                                                "minor : " + data['minor'] + "\n" +
                                                 "rssi : " + data['rssi'] + "\n" +
                                                 "event_stage : " + data['event_stage'],
                                         "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
