eae28d0695bd3ff0bfc4694643d1c28942a6ade7
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-05-25 13:52:44 +0700

    edit android debig

--- hooking.py
+++ hooking.py
@@ -60,15 +60,7 @@ class Hooking(Resource):
 
         # if(data['uuid'] == "C8A94F42-3CD5-483A-8ADC-97473197B8B4"):
         if('uuid' in data):
-            print(data['event_stage'])
-            print(data['major'])
-            print(data['minor'])
-            print(data['platform'])
-            print(data['rssi'])
-            print(data['timestamp'])
-            print(data['user_latitude'])
-            print(data['user_longitude'])
-            print(data['uuid'])
+            
             update_url = 'https://petdy-dev.one.th/api/beacon_update_location'
             update_body = {
                                 "event_stage":data['event_stage'],
@@ -98,7 +90,9 @@ class Hooking(Resource):
                                                 "---------------------------" + "\n" +
                                                 "uuid : " + data['uuid'] + "\n" +
                                                 "major : " + data['major'] + "\n" + 
-                                                "minor : " + data['minor'] + "\n",
+                                                "minor : " + data['minor'] + "\n"
+                                                "rssi : " + data['rssi'] + "\n" +
+                                                "event_stage : " + data['event_stage'],
                                         "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
                                     }
             if 'ios' in data['platform']:
