568a0e7d686bb036dcecd554cb782945c6c489cb
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-06-02 14:38:51 +0700

    covid json

--- hooking.py
+++ hooking.py
@@ -58,12 +58,15 @@ class Hooking(Resource):
             self.get_userprofile_body = { "one_id":  data['oneid'] }
             userprofile = requests.post(self.get_userprofile_api, json=self.get_userprofile_body, verify=False)
             print('this is user profile : ' + json.dumps(userprofile.json()))
+            user_data = userprofile.json()['user_data']
 
             chekcovid = requests.post(self.covid_api, json=covid_body, verify=False)
             covid_filter = filter(self.date_filter, chekcovid.json())
             for covid_status in covid_filter:
                 if 'green'in covid_status['status']:
-                    # check_in = self.check_in('')
+                    check_in = self.check_in('', json.dumps(user_data['oneid']), datetime.today().strftime('%Y-%m-%d'), datetime.today().strftime('%Y-%m-%d'), covid_status['status'])
+                    print(TAG, "check_in", check_in)
+                    
                     self.user_data['covid_tracking'] = json.dumps(covid_status['status'])
                     print('this is user_data : ' + json.dumps(self.user_data))
                     print(json.dumps(self.user_data['covid_tracking']))
