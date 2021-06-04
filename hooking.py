f81aad292c323199b39c6ef9602d92ce199d687b
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-04 08:23:53 +0700

    test_63-79

--- hooking.py
+++ hooking.py
@@ -65,7 +65,7 @@ class Hooking(Resource):
 
         if ('event' in data):
             print(data['event'])
-            if(data['event'] == 'message' and data['message']['text'] == "Hi"):
+            if(data['event'] == 'message' and data['message']['text'] == 'Hi'):
                    sendmessage_body = {
                                         "to": data['source']['one_id'],
                                         "bot_id": self.beaconbot_id,
@@ -80,15 +80,15 @@ class Hooking(Resource):
 
         # sendmessage_headers = {"Authorization": self.onechat_dev_token, "Content-Type": "application/json"}
         # sendmessage_url = 'https://chat-api.one.th/message/api/v1/push_message'
-        sendmessage_body = {
-                                    "to": data['oneid'],
-                                    "bot_id": "B7f2abd3c4e0e57dbb5c71bfa43920b5a",
-                                    "type": "text",
-                                    "message": data['payload'],
-                                    "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
-        }
-        sendmessage = requests.post(sendmessage_url, json=sendmessage_body, headers=sendmessage_headers, verify=False)
-        print("debug onechat response :" + json.dumps(sendmessage.json()))
+        # sendmessage_body = {
+        #                             "to": data['oneid'],
+        #                             "bot_id": "B7f2abd3c4e0e57dbb5c71bfa43920b5a",
+        #                             "type": "text",
+        #                             "message": data['payload'],
+        #                             "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
+        # }
+        # sendmessage = requests.post(sendmessage_url, json=sendmessage_body, headers=sendmessage_headers, verify=False)
+        # print("debug onechat response :" + json.dumps(sendmessage.json()))
 
     #petdy_iBEACON
         # if('uuid' in data):
