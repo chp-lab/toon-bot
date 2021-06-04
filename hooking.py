7c66d237c5e47b137b9b5a3aa3f6d987cbde1b67
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-04 10:07:23 +0700

    edit 81

--- hooking.py
+++ hooking.py
@@ -78,7 +78,7 @@ class Hooking(Resource):
             print(data['event'])
             if(data["event"]=='message'):
                 sendmessage_body = {
-                                    "to":['source']['oneid'],
+                                    "to":data['source']['oneid'],
                                     "bot_id": self.beaconbot_id,
                                     "type": "text",
                                     "message": "Say,Hi 555",
