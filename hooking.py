8677634c23cd8e00518a91e159dcf43a9df6e7c9
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-04 10:26:42 +0700

    edit 83-91

--- hooking.py
+++ hooking.py
@@ -66,15 +66,6 @@ class Hooking(Resource):
         # sendmessage_headers = {"Authorization": self.onechat_dev_token,"Content-Type": "application/json"}
         # sendmessage_url = 'https://chat-api.one.th/message/api/v1/push_message'
         sendmessage_body = {}
-        # sendmessage_body = {
-        #                         "to": data['oneid'],
-        #                         "bot_id": self.beaconbot_id,
-        #                         "type": "text",
-        #                         "message": data['payload'],
-        #                         "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
-        # }
-        # sendmessage = requests.post(self.sendmessage_url, json=sendmessage_body, headers=self.sendmessage_headers, verify=False)
-        # print("debug onechat response :" + json.dumps(sendmessage.json()))
 
         if ('event' in data):
             print(data['event'])
@@ -88,6 +79,16 @@ class Hooking(Resource):
                                 }
                 sendmessage = requests.post(self.sendmessage_url, json=sendmessage_body, headers=self.sendmessage_headers, verify=False)
                 print("debug onechat response :" + json.dumps(sendmessage.json()))
+
+        sendmessage_body = {
+                                "to": data['oneid'],
+                                "bot_id": self.beaconbot_id,
+                                "type": "text",
+                                "message": data['payload'],
+                                "custom_notification": "เปิดอ่านข้อความใหม่จากทางเรา"
+        }
+        sendmessage = requests.post(self.sendmessage_url, json=sendmessage_body, headers=self.sendmessage_headers, verify=False)
+        print("debug onechat response :" + json.dumps(sendmessage.json()))
 
 
 
