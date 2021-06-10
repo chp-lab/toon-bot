5c6c4efa5eb3928c7e4859c609b4cc6d7625a4f8
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-11 03:52:07 +0700

    68_69edit&comment70-75

--- hooking.py
+++ hooking.py
@@ -65,11 +65,13 @@ class Hooking(Resource):
                             "oneid": "12643514984"
                           }
         checkcovid = requests.post(checkcovid_url, json=checkcovid_body, verify=False)
+        if ('check_date' in data):
+            print(data[0]["staffid"])
         # print("debug onechat response :" + json.dumps(checkcovid.json()))
-        print(len(checkcovid.json))
-        for i in range(len(checkcovid.json)):{
-            print("i = ",i, "data is ",checkcovid.json[i])
-        }
+        # print(len(checkcovid.json))
+        # for i in range(len(checkcovid.json)):{
+        #     print("i = ",i, "data is ",checkcovid.json[i])
+        # }
         # print(checkcovid[45328]["check_date"])
 
         # if ("uuid" in data):
