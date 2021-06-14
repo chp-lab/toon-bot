e0ef40b9f8d55a6f06c2b711ee67074f778bb866
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-14 12:13:48 +0700

    edit

--- hooking.py
+++ hooking.py
@@ -191,7 +191,7 @@ class Hooking(Resource):
                 "oneid": "12643514984"
             }
         user_profile = requests.post(self.get_userprofile_api, json=self.get_userprofile_body, verify=False)
-        print("this is user profile : " + json.dumps(user_profile.json()["result"][0][1]))
+        print("this is user profile : " + json.dumps(user_profile.json()["result"][0]["one_id"]))
 
         return {
             "type": True,
