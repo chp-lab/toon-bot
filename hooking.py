dfc9aae7f305dd0f0de5aec3f6c952dd938a54e4
silas-decoder00714 <ricksiras14@gmail.com>
2021-06-15 11:58:45 +0700

    edit_test87-100

--- hooking.py
+++ hooking.py
@@ -15,6 +15,7 @@ class Hooking(Resource):
     beaconbot_id = "B7f2abd3c4e0e57dbb5c71bfa43920b5a"
     onechat_uri = "https://chat-api.one.th"
     onechat_dev_token = "Bearer Af047823219745b05b6993360704664914fff808c0a544edfa73dbec65d8daebf59ea0ed141bd4d93811a798db510b5c8"
+    bot_id = "B7f2abd3c4e0e57dbb5c71bfa43920b5a"
     # onechat_url1 = onechat_uri + '/message/api/v1/push_quickreply'
     covid_api = "https://hr-management.inet.co.th:5000/detail_user_data"
     covid_body = {}
@@ -27,7 +28,7 @@ class Hooking(Resource):
 
     # check_out_api = "http://203.151.164.229:5009/api/v1/check_out"
     # check_out_body = {}
-
+    headers = {"Authorization": onechat_dev_token, "Content-Type": "application/json"}
     sendmessage_headers = {"Authorization": onechat_dev_token}
     sendmessage_url = 'https://chat-api.one.th/message/api/v1/push_message'
     sendmessage_body ={}
@@ -82,6 +83,21 @@ class Hooking(Resource):
 
         print("this is data :" + json.dumps(data))
         print("this is header :"+request.headers)
+
+        auth_key = "Authorization"
+        if(auth_key not in request.headers):
+            return module.unauthorized()
+
+        auth = request.headers.get("Authorization")
+        print(TAG, "auth=", auth)
+        payload = {
+            "bot_id":self.bot_id,
+            "source":auth
+        }
+
+        r = requests.post(self.onechat_uri + "/manage/api/v1/getprofile", headers=self.headers, json=payload)
+        print(TAG, "response code=", r.status_code)
+        print(TAG, r.json())
 
         # checkcovid_url = 'https://hr-management.inet.co.th:5000/detail_user_data'
         # checkcovid_body = {
