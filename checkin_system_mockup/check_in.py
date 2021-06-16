d38ffc5b09719da701c85104ff88b6574d668918
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-06-16 17:12:35 +0700

    bug fix

--- checkin_system_mockup/check_in.py
+++ checkin_system_mockup/check_in.py
@@ -9,7 +9,26 @@ import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Check_in(Resource):
+    def is_entred(self, one_id):
+        TAG = "is_entered:"
+        cmd = """SELECT timeattendance.log_id, timeattendance.one_email, timeattendance.check_in 
+        FROM `timeattendance` 
+        WHERE timeattendance.employee_code='%s' AND timeattendance.date=CURRENT_DATE""" %(one_id)
+        database = Database()
+        res = database.getData(cmd)
+        print(TAG, res)
+        if(res[0]['len'] == 0):
+            return False
+        else:
+            return True
+
     def check_in(self, one_email, one_id, check_in_time, covid_tracking, date):
+        TAG = "check_in:"
+        module = Module()
+        if(self.is_entred(one_id)):
+            print(TAG, "user was enter")
+            # end job when record is exist!
+            return module.wrongAPImsg()
         database = Database()
         sql = """INSERT INTO `timeattendance` (`one_email`, `employee_code`, `check_in`, `covid_tracking`, `date`) VALUES ('%s', '%s', '%s', '%s', '%s')""" \
               % (one_email, one_id, check_in_time, covid_tracking, date)
