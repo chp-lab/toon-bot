804f702a6b64066da3df2913eacb53083e9003f7
jitlada <tang2538a@gmail.com>
2021-06-21 11:40:20 +0700

    change server port to 5008 dev port

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ from users import Users
 # from monitor import Monitor
 from covid_log import Covid_log
 
+
 class Server:
     app = None
     api = None
@@ -44,11 +45,14 @@ if (__name__ == "__main__"):
     # server.api.add_resource(Qr_decode, API_VERSION + "/myqr")
     # server.api.add_resource(My_mqtt, API_VERSION + "/unlock/<room_num>")
     server.api.add_resource(Covid_log, API_VERSION + "/covid/log")
-    server.api.add_resource(Timeattendance, API_VERSION + "/admin/get_timeattendance")
-    server.api.add_resource(Custommessage, API_VERSION + "/admin/custommessage")
-    server.api.add_resource(Replymessage, API_VERSION + "/admin/get_replymessage")
+    server.api.add_resource(
+        Timeattendance, API_VERSION + "/admin/get_timeattendance")
+    server.api.add_resource(
+        Custommessage, API_VERSION + "/admin/custommessage")
+    server.api.add_resource(Replymessage, API_VERSION +
+                            "/admin/get_replymessage")
     server.api.add_resource(Userprofile, API_VERSION + "/get_userprofile")
     server.api.add_resource(Check_in, API_VERSION + "/check_in")
     server.api.add_resource(Check_out, API_VERSION + "/check_out")
     server.api.add_resource(Users, API_VERSION + "/profile")
-    server.app.run(host="0.0.0.0", debug=True, port=5007)
+    server.app.run(host="0.0.0.0", debug=True, port=5008)
