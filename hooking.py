9e47483e4af8e1dbe68ce71ab78364aaaaba7830
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-06-24 17:04:23 +0700

    update location

--- hooking.py
+++ hooking.py
@@ -78,6 +78,15 @@ class Hooking(Resource):
             print('test async')
 
         if('uuid' in data):
+            def get_or_create_eventloop():
+                try:
+                    return asyncio.get_event_loop()
+                except RuntimeError as ex:
+                    if "There is no current event loop in thread" in str(ex):
+                        loop = asyncio.new_event_loop()
+                        asyncio.set_event_loop(loop)
+                        return asyncio.get_event_loop()
+
             async def diss(lis):
                 x = []
                 for i in lis:
@@ -90,8 +99,8 @@ class Hooking(Resource):
                 res = await diss(lis)
                 return res
 
-
-            loop = asyncio.get_event_loop()
+            
+            loop = get_or_create_eventloop()
             res = loop.run_until_complete(main())
             print("this is res" + res)
 
