0c9f622be6bfc8074c590341dd260f4ce47c58b3
jitlada <tang2538a@gmail.com>
2021-07-01 10:05:27 +0700

    edit

--- report/export.py
+++ report/export.py
@@ -219,7 +219,7 @@ class Export_excel(Resource):
         filename = request.args['file_name']
         filepath = request.args['file_path']
         if filename is not None and filepath is not None:
-            excel_file = self.download_file(filename, filepath)
+            excel_file = self.download_file(filepath, filename)
         #     return excel_file
         # else:
         #     return "Not OK"
