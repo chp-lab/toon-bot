b14bc27569f7aedf7d51c62a528d29482cdc482d
ฉัตรเพชร เคนานัน (CHATPETH KENANAN) <5610110106@email.psu.ac.th>
2021-09-15 17:45:55 +0700

    add http server

---
+++ httpserver.py
@@ -0,0 +1,61 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from booking import Booking
+from check_perm import Check_perm
+from hooking import Hooking
+from qr_decode import Qr_decode
+from my_mqtt import My_mqtt
+from admin.get_timeattendance import Timeattendance
+from admin.custommessage import Custommessage
+from admin.get_Replymessage import Replymessage
+from checkin_system_mockup.get_userprofile import Userprofile
+from checkin_system_mockup.check_in import Check_in
+from checkin_system_mockup.update_checkout import Check_out
+from users import Users
+from report.export import Export_excel
+from admin.get_access_log import Access_log
+from admin.get_room_number import Room_number
+
+# from monitor import Monitor
+from covid_log import Covid_log
+
+class Server:
+    app = None
+    api = None
+    meter = None
+    # monitor = None
+
+    def __init__(self):
+        print("init")
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.api = Api(self.app)
+
+
+if (__name__ == "__main__"):
+    TAG = "main:"
+    API_VERSION = "/api/v1"
+    server = Server()
+    # monitor = Monitor()
+    # my_mqtt = My_mqtt()
+
+    # server.api.add_resource(Check_perm, API_VERSION + "/check_perm/<room_num>")
+    # server.api.add_resource(Booking, API_VERSION + "/booking/<booking_number>")
+    server.api.add_resource(Hooking, API_VERSION + "/hooking")
+    # server.api.add_resource(Qr_decode, API_VERSION + "/myqr")
+    server.api.add_resource(My_mqtt, API_VERSION + "/unlock/<room_num>")
+    server.api.add_resource(Covid_log, API_VERSION + "/covid/log")
+    server.api.add_resource(Timeattendance, API_VERSION + "/admin/get_timeattendance")
+    server.api.add_resource(Custommessage, API_VERSION + "/admin/custommessage")
+    server.api.add_resource(Replymessage, API_VERSION + "/admin/get_replymessage")
+    server.api.add_resource(Userprofile, API_VERSION + "/get_userprofile")
+    server.api.add_resource(Check_in, API_VERSION + "/check_in")
+    server.api.add_resource(Check_out, API_VERSION + "/check_out")
+    server.api.add_resource(Users, API_VERSION + "/profile")
+    server.api.add_resource(Export_excel, API_VERSION + "/report/export")
+    server.api.add_resource(Access_log, API_VERSION + "/admin/get_access_log")
+    server.api.add_resource(Room_number, API_VERSION + "/admin/get_room_number")
+    server.app.run(host="0.0.0.0", debug=True, port=5007)
+    # context = ('/etc/letsencrypt/live/www.giantiot.com/fullchain.pem', '/etc/letsencrypt/live/www.giantiot.com/privkey.pem')
+    # server.app.run(host="www.giantiot.com", debug=True, ssl_context=context, port=5008)
